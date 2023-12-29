0bde39e16f84248fca4f35d17711466c5411b599
BrandonsCode <brandonscpucode@gmail.com>
2023-12-29 18:42:48 -0500

    test

---
+++ assets/common/baseUrl.js
@@ -0,0 +1,13 @@
+import { Platform } from 'react-native'
+
+let baseURL = 'https://b0ea-2603-3001-109d-2100-1dd9-b078-e285-bd5.ngrok-free.app/api/v1/'
+
+// let baseURL = '';
+
+// {Platform.OS == 'android'
+// ? baseURL = 'http://10.1.10.21:3000/api/v1/'
+
+// : baseURL = 'http://localhost:3000/api/v1/'
+// }
+
+export default baseURL;
